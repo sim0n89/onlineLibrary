95a71670c51aa7dd482e6ec8613f213cde540e2e
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2023-09-05 14:44:17 +0300

    обновил readme

--- render_website.py
+++ render_website.py
@@ -16,7 +16,7 @@ def on_reload():
     )
 
     book_chunks = list(more_itertools.chunked(books, 10))
-    page_count=len(book_chunks)
+    page_count = len(book_chunks)
     for page_number, book_list in enumerate(book_chunks, 1):
         os.makedirs("pages", exist_ok=True)
         file_path = os.path.join("pages", f"index{page_number}.html")
@@ -29,13 +29,9 @@ def on_reload():
         )
         with open(file_path, "w", encoding="utf-8") as output_file:
             output_file.write(rendered_html)
+
+
 on_reload()
 server = Server()
-
 server.watch("templates/*.html", on_reload)
-
 server.serve()
-
-
-if __name__ == "__main__":
-    on_reload()
